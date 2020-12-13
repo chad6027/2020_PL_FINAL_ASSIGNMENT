4df4b2c4cf8ed40446ee6427473932acef636b4b
chad6027 <chad6027@gmail.com>
2020-12-13 23:59:34 +0900

    day 02 Design

---
+++ Client.java
@@ -0,0 +1,18 @@
+package aroundearth;
+
+public class Client {
+
+	public void enterGame() {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
---
+++ Server.java
@@ -0,0 +1,20 @@
+package aroundearth;
+
+public class Server {
+	
+	public void waitForClient() {
+		
+	}
+	
+	public int vote() {
+		
+		return 0;
+	}
+	
+	public void setScrull() {
+		
+	}
+	
+	
+	
+}

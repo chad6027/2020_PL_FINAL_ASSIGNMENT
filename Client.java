626f3adbbc7e1606bcdf122fe7a454c9f78df819
chad6027 <chad6027@gmail.com>
2020-12-14 17:15:06 +0900

    day 03 design & pseudo code

--- Client.java
+++ Client.java
@@ -1,18 +1,45 @@
 package aroundearth;
 
-public class Client {
+public class Client implements Runnable{
 
-	public void enterGame() {
+	public static void enterGame() {
+		
+	}
+	
+	public static void vote() {
 		
 	}
 	
 	
-	
 	public static void main(String[] args) {
+		// if 방 참가 
+		// port 번호 입력 후 enterGame()
 		
+		// if 방 만들기
+		// Server를 쓰레드로 생성
+		// 랜덤 생성한 port 번호를 이용하여 자동 참가
 		
+		// 게임이 종료될 때까지 반복
+		while(true) {
 		
+			// 제한된 시간동안 입력
+			
+			vote();
+			
+			
+		}
 		
+	}
+	
+
+	
+
+
+
+	@Override
+	// 출력 스트림 분리
+	public void run() {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- Server.java
+++ Server.java
@@ -1,20 +1,50 @@
 package aroundearth;
 
-public class Server {
+public class Server implements Runnable{
+	
 	
 	public void waitForClient() {
 		
 	}
 	
-	public int vote() {
+	public int getVoting() {
 		
 		return 0;
 	}
 	
-	public void setScrull() {
+	public void setSkrull() {
 		
 	}
-	
-	
-	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i = 0 ; i < 5; ++i) {
+			waitForClient();
+		}
+		
+		setSkrull();
+		
+		
+		while(true) {
+
+			// Client에게 채팅 시간 부여
+			
+			getVoting();
+			
+			// Game 종료 확인
+			
+		}
+	}
+}
+
+class ChatThread implements Runnable{
+
+	@Override
+	// 입출력을 여기서 처리
+	// 서버는 듣고 보내주기만 하면 된다.
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }

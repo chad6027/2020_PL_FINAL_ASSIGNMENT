354104720d188cac596bf58be790c1c503df0dec
chad6027 <chad6027@gmail.com>
2020-12-15 23:36:40 +0900

    day 04 Filling out the main()

--- Client.java
+++ Client.java
@@ -1,8 +1,11 @@
 package aroundearth;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Client implements Runnable{
 
-	public static void enterGame() {
+	public static void enterGame(int port) {
 		
 	}
 	
@@ -12,12 +15,35 @@ public class Client implements Runnable{
 	
 	
 	public static void main(String[] args) {
-		// if 방 참가 
-		// port 번호 입력 후 enterGame()
-		
-		// if 방 만들기
-		// Server를 쓰레드로 생성
-		// 랜덤 생성한 port 번호를 이용하여 자동 참가
+		Scanner scv = new Scanner(System.in);
+			
+		while(true) {
+			System.out.printf("1. 방 참가\n2. 방만들기");
+			int particpate = scv.nextInt();
+			int port;
+			
+			if(particpate == 1) {
+				// port 번호 입력 후 enterGame()
+				System.out.print("Port : ");
+				port = scv.nextInt();
+				
+				enterGame(port);
+				
+				break;
+			}
+			else if(particpate == 2) {
+				// 랜덤 생성한 port 번호를 이용하여 자동 참가
+				Random rand = new Random();
+				port = 8000 + rand.nextInt(2000);
+				System.out.print("New Room's Port number : " + port);
+				Server new_server = new Server(port);
+				// Server를 쓰레드로 생성
+				
+				break;
+			}
+			else 
+				System.out.printf("옳지 않은 값을 입력하셨습니다");
+		}
 		
 		// 게임이 종료될 때까지 반복
 		while(true) {
--- Server.java
+++ Server.java
@@ -2,7 +2,12 @@ package aroundearth;
 
 public class Server implements Runnable{
 	
+	public int port;
 	
+	public Server(int port) {
+		this.port = port;
+	}
+	// client를 받아 hashmap<String, Socket>에 추가
 	public void waitForClient() {
 		
 	}
@@ -20,7 +25,7 @@ public class Server implements Runnable{
 	public void run() {
 		// TODO Auto-generated method stub
 		for(int i = 0 ; i < 5; ++i) {
-			waitForClient();
+			waitForClient(); 
 		}
 		
 		setSkrull();
@@ -36,15 +41,21 @@ public class Server implements Runnable{
 			
 		}
 	}
-}
-
-class ChatThread implements Runnable{
-
-	@Override
-	// 입출력을 여기서 처리
-	// 서버는 듣고 보내주기만 하면 된다.
-	public void run() {
-		// TODO Auto-generated method stub
+	
+	public void sendMessage() {
 		
 	}
+	
+	//-------------------------------------------
+	class ChatThread implements Runnable{
+
+		@Override
+		// 입출력을 여기서 처리
+		// 서버는 듣고 보내주기만 하면 된다.
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
 }
+
